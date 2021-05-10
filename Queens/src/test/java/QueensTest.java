0f8e7c8088b375e1a206e2dedb624550d6d5e978
JuanTrebolle <juan.itrebolle@gmail.com>
2021-05-10 22:32:17 +0200

    queens test done

--- Queens/src/test/java/QueensTest.java
+++ Queens/src/test/java/QueensTest.java
@@ -1,23 +1,48 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class QueensTest {
     private int[][] testArray;
+    private Queens queens;
 
     @BeforeEach
     void setup(){
-        testArray = new int[3][3];
+        testArray = new int[4][4];
+        queens = new Queens(5);
     }
 
     @AfterEach
     void tearDown(){
         testArray = null;
     }
-    
-    void printBoard(){
-        for(int i = 0; i < testArray.length; i++){
-            for (int j = 0; j < testArray.length; j++) {
-                System.out.println(testArray[i][j]);
+
+    private int[][] buildBoard(){
+        testArray = new int [][]{{1, 0, 0, 0, 0},
+            {0, 0, 0, 1, 0},
+            {0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 1},
+            {0, 0, 1, 0, 0}};
+        return testArray;
+    }
+
+    @Test
+    void checkBoard(){
+        queens.calculate();
+        System.out.println(queens);
+        assertTrue(Arrays.deepEquals(buildBoard(), queens.calculate()));
+        printBoard(queens.calculate());
+    }
+
+
+    void printBoard(int array[][]){
+        for(int i = 0; i < array.length; i++){
+            for (int j = 0; j < array.length; j++) {
+                System.out.println(array[i][j]);
             }
         }
     }

c3e6f5d8a9e40e774bbce0bfe8b34d0f7568224b
JuanTrebolle <juan.itrebolle@gmail.com>
2021-05-10 20:09:52 +0200

    setup test

--- Queens/src/test/java/QueensTest.java
+++ Queens/src/test/java/QueensTest.java
@@ -1,2 +1,24 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
 public class QueensTest {
+    private int[][] testArray;
+
+    @BeforeEach
+    void setup(){
+        testArray = new int[3][3];
+    }
+
+    @AfterEach
+    void tearDown(){
+        testArray = null;
+    }
+    
+    void printBoard(){
+        for(int i = 0; i < testArray.length; i++){
+            for (int j = 0; j < testArray.length; j++) {
+                System.out.println(testArray[i][j]);
+            }
+        }
+    }
 }
